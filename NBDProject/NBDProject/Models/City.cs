951f24bff5625375501bae3d87b4a341765b066a
jakelong0509 <34384740+jakelong0509@users.noreply.github.com>
2017-12-26 16:33:56 -0500

    Edit City Model

--- NBDProject/NBDProject/Models/City.cs
+++ NBDProject/NBDProject/Models/City.cs
@@ -12,6 +12,10 @@ namespace NBDProject.Models
 {
     public class City
     {
+        public City()
+        {
+            this.Clients = new HashSet<Client>();
+        }
         public int ID { get; set; }
 
         [Display(Name = "City")]
@@ -19,7 +23,7 @@ namespace NBDProject.Models
         [StringLength(50, ErrorMessage = "City name can be no longer then 50 characters")]
         public string city { get; set; }
 
-        public virtual Client client { get; set; }
+        public virtual ICollection<Client> Clients { get; set; }
 
     }
 }

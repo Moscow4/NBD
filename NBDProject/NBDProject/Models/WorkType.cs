d4323d12a44fe2c2d09c125a326a4e4f580242ef
Rainan1 <sabersftw777@gmail.com>
2017-12-26 17:14:25 -0500

    Worker,Tool,WorkerType
    added name, required, stringlength and Datatype to worker, workerType and Tool

--- NBDProject/NBDProject/Models/WorkType.cs
+++ NBDProject/NBDProject/Models/WorkType.cs
@@ -12,8 +12,23 @@ namespace NBDProject.Models
     public class WorkType
     {
         public int ID { get; set; }
+
+        [Display(Name = "Worker Type Description")]
+        [Required(ErrorMessage = "Enter Worker Type Description")]
+        [StringLength(50, ErrorMessage = "Worker Type Description cannot be more than 50 characters.")]
         public string workTypeDesc { get; set; }
-        public int workTypePrice { get; set; }
-        public int workTypeCost { get; set; }
+
+        [Display(Name = "Worker Type Price")]
+        [Required(ErrorMessage = "Enter Worker Type Price")]
+        [Range(0.01, 9999.99, ErrorMessage = "Invalid Price.")]
+        [DataType(DataType.Currency)]
+        public decimal workTypePrice { get; set; }
+
+
+        [Display(Name = "Worker Type Cost")]
+        [Required(ErrorMessage = "Enter Worker Type Cost")]
+        [Range(0.01, 9999.99, ErrorMessage = "Invalid Cost.")]
+        [DataType(DataType.Currency)]
+        public decimal workTypeCost { get; set; }
     }
 }

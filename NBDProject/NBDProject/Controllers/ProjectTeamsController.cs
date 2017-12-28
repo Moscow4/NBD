6417b2b0d1e5c7fb9ccdcd3e9c0f0b73dd6b241e
OnyxFlames <jacobhicks1996@hotmail.com>
2017-12-27 22:41:06 -0500

    All of my(Jake's) controllers generated.

---
+++ NBDProject/NBDProject/Controllers/ProjectTeamsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NBDProject.DAL;
+using NBDProject.Models;
+
+namespace NBDProject.Controllers
+{
+    public class ProjectTeamsController : Controller
+    {
+        private NBDCFEntities db = new NBDCFEntities();
+
+        // GET: ProjectTeams
+        public ActionResult Index()
+        {
+            return View(db.ProjectTeams.ToList());
+        }
+
+        // GET: ProjectTeams/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectTeam projectTeam = db.ProjectTeams.Find(id);
+            if (projectTeam == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projectTeam);
+        }
+
+        // GET: ProjectTeams/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProjectTeams/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,teamPhaseIn,projectID")] ProjectTeam projectTeam)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProjectTeams.Add(projectTeam);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(projectTeam);
+        }
+
+        // GET: ProjectTeams/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectTeam projectTeam = db.ProjectTeams.Find(id);
+            if (projectTeam == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projectTeam);
+        }
+
+        // POST: ProjectTeams/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,teamPhaseIn,projectID")] ProjectTeam projectTeam)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(projectTeam).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(projectTeam);
+        }
+
+        // GET: ProjectTeams/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectTeam projectTeam = db.ProjectTeams.Find(id);
+            if (projectTeam == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projectTeam);
+        }
+
+        // POST: ProjectTeams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProjectTeam projectTeam = db.ProjectTeams.Find(id);
+            db.ProjectTeams.Remove(projectTeam);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ NBDProject/NBDProject/Controllers/WorkersController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NBDProject.DAL;
+using NBDProject.Models;
+
+namespace NBDProject.Controllers
+{
+    public class WorkersController : Controller
+    {
+        private NBDCFEntities db = new NBDCFEntities();
+
+        // GET: Workers
+        public ActionResult Index()
+        {
+            var workers = db.Workers.Include(w => w.WorkType);
+            return View(workers.ToList());
+        }
+
+        // GET: Workers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Worker worker = db.Workers.Find(id);
+            if (worker == null)
+            {
+                return HttpNotFound();
+            }
+            return View(worker);
+        }
+
+        // GET: Workers/Create
+        public ActionResult Create()
+        {
+            ViewBag.worktypeID = new SelectList(db.WorkTypes, "ID", "workTypeDesc");
+            return View();
+        }
+
+        // POST: Workers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,FName,LName,worktypeID")] Worker worker)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Workers.Add(worker);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.worktypeID = new SelectList(db.WorkTypes, "ID", "workTypeDesc", worker.worktypeID);
+            return View(worker);
+        }
+
+        // GET: Workers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Worker worker = db.Workers.Find(id);
+            if (worker == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.worktypeID = new SelectList(db.WorkTypes, "ID", "workTypeDesc", worker.worktypeID);
+            return View(worker);
+        }
+
+        // POST: Workers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,FName,LName,worktypeID")] Worker worker)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(worker).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.worktypeID = new SelectList(db.WorkTypes, "ID", "workTypeDesc", worker.worktypeID);
+            return View(worker);
+        }
+
+        // GET: Workers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Worker worker = db.Workers.Find(id);
+            if (worker == null)
+            {
+                return HttpNotFound();
+            }
+            return View(worker);
+        }
+
+        // POST: Workers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Worker worker = db.Workers.Find(id);
+            db.Workers.Remove(worker);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

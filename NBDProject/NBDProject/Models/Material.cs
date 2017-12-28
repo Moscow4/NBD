a602afc0d1fc512510ce9f3c7060a6da2852f1eb
OnyxFlames <jacobhicks1996@hotmail.com>
2017-12-27 19:46:28 -0500

    Added attributes to Inventory and Material model classes

--- NBDProject/NBDProject/Models/Material.cs
+++ NBDProject/NBDProject/Models/Material.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,16 @@ namespace NBDProject.Models
 {
     public class Material
     {
+        [Required(ErrorMessage = "Material ID is needed!")]
+        [Display(Name = "Material")]
         public int ID { get; set; }
 
+        [Required(ErrorMessage = "Material description cannot be left blank!")]
+        [Display(Name = "Description")]
         public string matDesc { get; set; }
 
+        [Display(Name = "Type")]
+        [Required(ErrorMessage = "Material type cannot be empty.")]
         public string matType { get; set; }
     }
 }

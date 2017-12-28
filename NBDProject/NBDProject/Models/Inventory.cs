1c44701dea4d14722ef7b1fa4121a6a0107c2262
OnyxFlames <jacobhicks1996@hotmail.com>
2017-12-27 19:11:01 -0500

    Added Material class [again]. Reverted Inventory to master's version.

--- NBDProject/NBDProject/Models/Inventory.cs
+++ NBDProject/NBDProject/Models/Inventory.cs
@@ -1,8 +1,4 @@
-﻿// Made By: Chris Haggerty
-// Date: 26/12/2017
-// Work: Setting up Get and Set
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,21 +7,22 @@ namespace NBDProject.Models
 {
     public class Inventory
     {
-        public Inventory()
-        {
-            //this.PlantInventories = new HashSet<PlantInventory>();
-            //this.PotteryInventories = new HashSet<PotteryInventory>();
-            //this.MaterialsInventories = new HashSet<MaterialsInventory>();
-
-        }
         public int ID { get; set; }
 
-        public string InvDescription { get; set; }
+        public decimal invAvgNet { get; set; }
 
-        
+        public int invList { get; set; }
+
+        public int invSizeAmnt { get; set; }
+
+        public string invSizeUnit { get; set; }
+
+        public string invQOH { get; set; }
+
+        public int materialID { get; set; }
+
+
         //one to many
-        //public virtual ICollection<PlantInventory> PlantInventories { get; set; }
-        //public virtual ICollection<PotteryInventory> PotteryInventories { get; set; }
-        //public virtual ICollection<MaterialsInventory> MaterialsInventories { get; set; }
+        public virtual Material Material { get; set; }
     }
 }
---
+++ NBDProject/NBDProject/Models/Material.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NBDProject.Models
+{
+    public class Material
+    {
+        public int ID { get; set; }
+
+        public string matDesc { get; set; }
+
+        public string matType { get; set; }
+    }
+}

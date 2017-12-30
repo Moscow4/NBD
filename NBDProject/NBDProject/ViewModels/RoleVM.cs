b5171c05c1360560e1fc4c4656a393edc9eb4ccb
jakelong0509 <34384740+jakelong0509@users.noreply.github.com>
2017-12-29 20:02:31 -0500

    M:M

---
+++ NBDProject/NBDProject/ViewModels/RoleVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NBDProject.ViewModels
+{
+    public class RoleVM
+    {
+        public string RoleID { get; set; }
+        public string RoleName { get; set; }
+        public bool Assigned { get; set; }
+    }
+}

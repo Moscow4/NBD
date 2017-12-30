4fb75aff8879ac77cb0bfca5b1b99d01a5a2d0d3
Rainan1 <sabersftw777@gmail.com>
2017-12-29 21:01:03 -0500

    added security
    seed data for roles and users and migrations to project

--- NBDProject/NBDProject/App_Start/IdentityConfig.cs
+++ NBDProject/NBDProject/App_Start/IdentityConfig.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using NBDProject.Models;
+using NBDProject.DAL;
 
 namespace NBDProject
 {
---
+++ NBDProject/NBDProject/DAL/SecurityMigrations/Configuration.cs
@@ -0,0 +1,114 @@
+namespace NBDProject.DAL.SecurityMigrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using NBDProject.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<NBDProject.DAL.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"DAL\SecurityMigrations";
+        }
+
+        protected override void Seed(NBDProject.DAL.ApplicationDbContext context)
+        {
+
+            var roleManager = new RoleManager<IdentityRole>(new
+                                       RoleStore<IdentityRole>(context));
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                var roleresult = roleManager.Create(new IdentityRole("Admin"));
+
+            }
+            if (!context.Roles.Any(r => r.Name == "Admin Assistant"))
+            {
+                var roleresult = roleManager.Create(new IdentityRole("Admin Assistant"));
+
+            }
+            if (!context.Roles.Any(r => r.Name == "Designer"))
+            {
+                var roleresult = roleManager.Create(new IdentityRole("Designer"));
+
+            }
+            if (!context.Roles.Any(r => r.Name == "Production Manager"))
+            {
+                var roleresult = roleManager.Create(new IdentityRole("Production Manager"));
+
+            }
+            if (!context.Roles.Any(r => r.Name == "Sales"))
+            {
+                var roleresult = roleManager.Create(new IdentityRole("Sales"));
+
+            }
+
+            var manager = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+
+            var adminuser = new ApplicationUser
+            {
+                UserName = "admin1@outlook.com",
+                // FavoriteIceCream = "Chocolate",
+                Email = "admin1@outlook.com"
+            };
+            var adminAssistantuser = new ApplicationUser
+            {
+                UserName = "adminAssistant1@outlook.com",
+                // FavoriteIceCream = "Chocolate",
+                Email = "adminAssistant1@outlook.com"
+            };
+
+            var productionManageruser = new ApplicationUser
+            {
+                UserName = "productionManager1@outlook.com",
+                // FavoriteIceCream = "Chocolate",
+                Email = "productionManager1@outlook.com"
+            };
+            var designeruser = new ApplicationUser
+            {
+                UserName = "designer1@outlook.com",
+                // FavoriteIceCream = "Chocolate",
+                Email = "designer1@outlook.com"
+            };
+
+            var salesuser = new ApplicationUser
+            {
+                UserName = "sales1@outlook.com",
+                // FavoriteIceCream = "Chocolate",
+                Email = "sales1@outlook.com"
+            };
+
+            if (!context.Users.Any(u => u.UserName == "admin1@outlook.com"))
+            {
+                manager.Create(adminuser, "password");
+                manager.AddToRole(adminuser.Id, "Admin");
+            }
+            if (!context.Users.Any(u => u.UserName == "adminAssistant1@outlook.com"))
+            {
+                manager.Create(adminAssistantuser, "password");
+                manager.AddToRole( adminAssistantuser.Id, "Admin Assistant");
+            }
+            if (!context.Users.Any(u => u.UserName == "designer1@outlook.com"))
+            {
+                manager.Create(designeruser, "password");
+                manager.AddToRole(designeruser.Id, "Designer");
+            }
+            if (!context.Users.Any(u => u.UserName == "sales1@outlook.com"))
+            {
+                manager.Create(designeruser, "password");
+                manager.AddToRole(designeruser.Id, "Sales");
+            }
+            if (!context.Users.Any(u => u.UserName == "productionManager1@outlook.com"))
+            {
+                manager.Create(designeruser, "password");
+                manager.AddToRole(designeruser.Id, "Production Manager");
+            }
+
+        }
+    }
+}

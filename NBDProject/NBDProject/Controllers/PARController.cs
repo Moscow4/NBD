0d22322df72501b8d6e280d6554470776e40dfec
jakelong0509 <34384740+jakelong0509@users.noreply.github.com>
2018-01-08 18:44:27 -0500

    PAR

---
+++ NBDProject/NBDProject/Controllers/PARController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NBDProject.ViewModels;
+using NBDProject.Models;
+using NBDProject.DAL;
+
+namespace NBDProject.Controllers
+{
+    public class PARController : Controller
+    {
+        private NBDCFEntities db = new NBDCFEntities();
+        // GET: PAR
+        public ActionResult Index()
+        {
+            var materialRequirement = (from m in db.MaterialRequirements
+                                       select m).ToList();
+            var labourRequirementDesign = (from ld in db.LabourRequirementDesigns
+                                           select ld).ToList();
+            var labourRequirement = (from l in db.LabourRequirements
+                                     select l).ToList();
+            var projectProductionStage = (from pp in db.Projects
+                                          where pp.projectFlagged == true
+                                          select pp).ToList();
+            var projectDesignBidStage = (from pd in db.Projects
+                                         where pd.projectFlagged == false
+                                         select pd).ToList();
+            var ViewModel = new PARVM
+            {
+                MaterialRequirement = materialRequirement,
+                LabourRequirementDesign = labourRequirementDesign,
+                LabourRequirement = labourRequirement,
+                ProjectDesignBidStage = projectDesignBidStage,
+                ProjectProductionStage = projectProductionStage
+
+            };
+            return View(ViewModel);
+        }
+    }
+}
---
+++ NBDProject/NBDProject/ViewModels/PARVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NBDProject.Models;
+using NBDProject.ViewModels;
+
+namespace NBDProject.ViewModels
+{
+    public class PARVM
+    {
+        public IList<Client> Client { get; set; }
+        public IList<Worker> Worker { get; set; }
+        public IList<Project> ProjectProductionStage { get; set; }
+        public IList<LabourRequirementDesign> LabourRequirementDesign { get; set; }
+        public IList<LabourRequirement> LabourRequirement { get; set; }
+        public IList<MaterialRequirement> MaterialRequirement { get; set; }
+        public IList<ProjectTool> ProjectTool { get; set; }
+        public IList<ProjectTeam> ProjectTeam { get; set; }
+        public IList<Project> ProjectDesignBidStage { get; set; }
+    }
+}
